11ff3a93b3cbdcf1d462862d999104a975d81a0a
POCHENG.LIN <pochieng0118@gmail.com>
2017-07-12 11:02:37 +0800

    'commit
    essage'

--- random_forest.py
+++ random_forest.py
@@ -1,3 +1,4 @@
+from sklearn import preprocessing
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import precision_score, \
     recall_score, classification_report, \
@@ -25,30 +26,43 @@ Cabin = pd.factorize(titanic_test['Cabin'])[0]
 titanic_test['Cabin'] = Cabin
 
 # testing set missing value filling
-titanic_test['Age'].fillna(value=0, inplace=True)
+titanic_test['Age'].fillna(value=titanic_test['Age'].mean(), inplace=True)
 
 # training set missing value filling
-titanic_train['Age'].fillna(value=0, inplace=True)
+titanic_train['Age'].fillna(value=titanic_train['Age'].mean(), inplace=True)
 print(titanic_train.head())
 #print(titanic_train.isnull().sum())
 
+scaler = preprocessing.MinMaxScaler()
+
+np_scaled = scaler.fit_transform(titanic_train)
+training_set__normalized = pd.DataFrame(np_scaled)
+
+np_scaled = scaler.fit_transform(titanic_test)
+testing_set__normalized = pd.DataFrame(np_scaled)
+
+print(training_set__normalized.head())
+
+
+
 # I delete the name & ticket columns
-tfeatures= titanic_test.columns[1:8]
-features = titanic_train.columns[2:9]
+tfeatures= testing_set__normalized.columns[1:8]
+features = training_set__normalized.columns[2:9]
 #print(titanic_train[features].head())
 
 y = titanic_train['Survived']
 
 # Training
 # Create a random forest classifier
-clf = RandomForestClassifier(n_jobs=2)
-
+clf = RandomForestClassifier(n_jobs=2, oob_score = True, n_estimators=2000, max_features=7,
+                             max_depth=4, random_state = 42, min_samples_leaf = 5)
+print(clf.get_params())
 # Train the classifier to take the training features and learn how they relate
 # to the training y (the species)
-clf.fit(titanic_train[features], y)
+clf.fit(training_set__normalized[features], y)
 
 # Apply the classifier we trained to the test data (which, remember, it has never seen before)
-prediction = clf.predict(titanic_train[features])
+prediction = clf.predict(training_set__normalized[features])
 
 #evaluation
 print ('Accuracy:', accuracy_score(y, prediction))
@@ -57,7 +71,7 @@ print ('Recall:', recall_score(y, prediction))
 print ('Precision:', precision_score(y, prediction))
 print ('\n classification report:\n', classification_report(y,prediction))
 
-test_predict = clf.predict(titanic_test[tfeatures])
+test_predict = clf.predict(testing_set__normalized[tfeatures])
 PassengerId = titanic_test['PassengerId']
 
 result = pd.DataFrame(PassengerId, columns=['PassengerId'])
